06841019b4f0ad3dda21e3ff18db180fc60b37ed
Yuxiao Shen <ys02211n@pace.edu>
2020-12-20 23:50:23 -0500

    update

--- Final Project/pi_lcd_weather/rpi-weather.py
+++ Final Project/pi_lcd_weather/rpi-weather.py
@@ -10,16 +10,16 @@ while i2c.try_lock():
 DEFAULT_I2C_ADDR = 0x27
 lcd = I2cLcd(i2c, DEFAULT_I2C_ADDR, 2, 16)
 
-#url = 'http://api.openweathermap.org/data/2.5/weather?id=5128581&appid=42404c2e7097f24b44a146ff6ebd4dfe'
-url = 'http://api.openweathermap.org/data/2.5/forecast/daily?id=5128581&cnt=7&appid=42404c2e7097f24b44a146ff6ebd4dfe'
+url = 'https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=en'
 
 while True:
     data = get(url).json()
-    temp_data = data('list', 'weather')
+    temp_data = data['temperature']['data']
     for entry in temp_data:
         lcd.clear()
         lcd.move_to(0, 0)
-        lcd.putstr(str(entry['main']))
-        #lcd.move_to(0, 1)
-        #lcd.putstr(entry['max'])
+        lcd.putstr(entry['place'][:15])
+        lcd.move_to(0, 1)
+        lcd.putstr(str(entry['value']))
+        lcd.putstr(str(entry['unit']))
         sleep(2)
